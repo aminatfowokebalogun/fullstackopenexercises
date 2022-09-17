b4a4935e0f958a1d48214672e1af558ac29e06a5
Balogun Aminat Fowoke <aminatbalogun00@gmail.com>
2022-09-17 07:20:34 +0100

    Ex1.6-1.14

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import React, {useState} from 'react';
-import Button from './Button';
-import Statistics from './Statistics';
 
 
 function App() {
-  //STATES
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0);
+  const [votecount, setVotecount] = useState(0);
 
-  //functions to add
-  const handleClickGood =()=>{setGood(good + 1)}
-  const handleClickBad =()=>{setBad(bad + 1)}
-  const handleClickNeutral =()=>{setNeutral(neutral + 1)}
-  
+  const handleRefresh =()=>{setSelected(selected + 1)}
+
+  const points = [1, 4, 6, 3]
+//variable to copy the points array
+  const copy = [...points]
+  // increment the value in position 2 by one
+  copy[selected] += 1
+
+  const handleVote =()=>{setVotecount(votecount + 1)}
+
 
   return (
     <>
-    <h1>
-      UNICAFE FEEDBACK PAGE
-    </h1>
-
-    <div>
-      <Button handleClick={handleClickGood} value={good} text='Rate Good'/><br/>
-      <Button handleClick={handleClickBad} value={bad} text='Rate Bad'/><br/>
-      <Button handleClick={handleClickNeutral} value={neutral} text='Rate Neutral'/>
-    </div>
-
-    <Statistics good={good} bad={bad} neutral={neutral}/>
+    {anecdotes[selected]}
+<br/>
+    <button onClick={handleRefresh}>
+      Refresh
+      </button>
+    <br/>
+      {votecount}
+      <br/>
+      <button onClick={handleVote}>
+        Vote Aecdote
+      </button>
     </>
   );
 }
